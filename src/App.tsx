ac1c0f0ac4e5a2ba036b8379035c145ab1519cf4
Loki Auto Bot (LoB) <loki@incognito-zone.xyz>
2026-04-03 02:36:41 +0000

    Preceding changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import WatchPage from "./pages/WatchPage";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
